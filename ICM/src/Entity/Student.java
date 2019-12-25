741341cc09d1cfc49396f84be1df0fc8ce35ffb1
aimanouda2309 <48187816+aimanouda2309@users.noreply.github.com>
2019-12-25 20:08:47 +0200

    15
    i wrote the entity package

---
+++ ICM/src/Entity/Student.java
@@ -0,0 +1,11 @@
+package Entity;
+
+public class Student extends User{
+	
+private int id;
+	
+public Student(String email, String password, String username) {
+	super(email, password, username);
+}
+
+}
--- ICM/src/Entity/User.java
+++ ICM/src/Entity/User.java
@@ -1,133 +1,52 @@
 package Entity;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
-import java.nio.file.Files;
-import java.util.ArrayList;
 
-import javafx.scene.image.Image;
 /**
  * The <code>Member</code> entity, represents a Member in the database.
  */
 public class User {
 
-	private String id;
 
-    private String phone;
-    private String email;
-
-    private boolean isConnected;
-    private Role role;
+    private String email;  
     private String password;
     private String username;
-    
     
     public User() {
     	
     }
     
 	
-
-    public User(String id, String phone, String email, boolean isConnected, Role role, String password,
-		String username) {
-	super();
-	this.id = id;
-	this.phone = phone;
+public User(String email, String password,String username) {
 	this.email = email;
-	this.isConnected = isConnected;
-	this.role = role;
 	this.password = password;
 	this.username = username;
 }
 
+public String getEmail() {
+	return email;
+}
 
+public void setEmail(String email) {
+	this.email = email;
+}
 
-	public String getId() {
-		return id;
-	}
-
-
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-
-
-	public String getPhone() {
-		return phone;
-	}
-
-
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-
-	public boolean isConnected() {
-		return isConnected;
-	}
-
-
-
-	public void setConnected(boolean isConnected) {
-		this.isConnected = isConnected;
-	}
-
-
-
-	public Role getRole() {
-		return role;
-	}
-
-
-
-	public void setPermission(Role role) {
-		this.role = role;
-	}
-
-
-
-	public String getPassword() {
+public String getPassword() {
 		return password;
-	}
+}
 
-
-
-	public void setPassword(String password) {
+public void setPassword(String password) {
 		this.password = password;
-	}
+}
 
-
-
-	public String getUsername() {
+public String getUsername() {
 		return username;
-	}
+}
 
+public void setUsername(String username) {
+	this.username = username;
+}
 
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-	public enum Role {
+public enum Role {
 	Studint, Secturer, Employee;
-    }
+}
 }
