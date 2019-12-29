e8222a653f57cfd0f9318deb2f0a86fefbe3bc47
samishekh <samisnow97@gmail.com>
2019-12-30 01:28:04 +0200

    ll

--- ICM/src/Boundary/LoginController.java
+++ ICM/src/Boundary/LoginController.java
@@ -1,22 +1,33 @@
 package Boundary;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import Client.ClientConsole;
+import Client.MainForClient;
+import Entity.Employee;
+import Entity.Inspector;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import ocsf.client.ObservableClient;
 /**
  * 
  * @author Ayman Odeh
  *
  */
-public class LoginController {
+public  class LoginController {
+@FXML
+private Label error;
 @FXML
 private Button Loginbtn;
 @FXML
@@ -28,15 +39,13 @@ private CheckBox Remember;
 @FXML
 private Button ForgetPass;
 
-private ClientConsole cc;
-public static Stage primaryStage;
-public LoginController(ClientConsole cc) {
+private static ClientConsole cc;
+public static  Stage primaryStage;
+
+public void start(Stage primaryStage,ClientConsole cc)  {
 	this.cc=cc;
-}
-public void start(Stage primaryStage)  {
 	try{	
-		    
-			Parent root = FXMLLoader.load(getClass().getResource("/Boundary/Login.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/Boundary/login.fxml"));
 			Scene scene =new Scene(root);
 			primaryStage.setScene(scene);
 		    primaryStage.setResizable(false);
@@ -58,6 +67,7 @@ public void start(Stage primaryStage)  {
  * @throws Exception
  */
 public void LoginAction(ActionEvent event) throws Exception{
+	error.setVisible(false);
 	String username=Username.getText();
 	String password=Password.getText();
 	String[] loginMessage=new String[3];
@@ -75,4 +85,14 @@ public void ForgetPassAction(ActionEvent event) throws Exception{
 	System.out.println("sssssss");
 	//open new window to get the password by enter the email address and then send link to reset the password
 }
+public void displayErrorMessage() {
+	error.setVisible(true);
+	Username.clear();
+	Password.clear();
+}
+public Stage getStage() {
+	return primaryStage;
+}
+
+
 }
