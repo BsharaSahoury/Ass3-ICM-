5c5a6c6100ced38615052a08b799a7d6f0ebbb57
aimanouda2309 <48187816+aimanouda2309@users.noreply.github.com>
2020-01-10 19:49:54 +0200

    yy

--- ICM/src/Entity/User.java
+++ ICM/src/Entity/User.java
@@ -1,133 +1,96 @@
 package Entity;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
 import java.io.Serializable;
-import java.nio.file.Files;
 import java.util.ArrayList;
 
-import javafx.scene.image.Image;
 /**
  * The <code>Member</code> entity, represents a Member in the database.
  */
-public class User {
+public class User implements Serializable {
 
-	private String id;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String password;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private ArrayList<Request> myRequests = new ArrayList<Request>(); 
+	public User() {
 
-    private String phone;
-    private String email;
-
-    private boolean isConnected;
-    private Role role;
-    private String password;
-    private String username;
-    
-    
-    public User() {
-    	
-    }
-    
-	
-
-    public User(String id, String phone, String email, boolean isConnected, Role role, String password,
-		String username) {
-	super();
-	this.id = id;
-	this.phone = phone;
-	this.email = email;
-	this.isConnected = isConnected;
-	this.role = role;
-	this.password = password;
-	this.username = username;
-}
-
-
-
-	public String getId() {
-		return id;
+	}
+	public User(String username,String firstName, String lastName) {
+		this.username=username;
+		this.firstName=firstName;
+		this.lastName=lastName;
 	}
 
-
-
-	public void setId(String id) {
-		this.id = id;
+	public User(String email, String password, String username, String firstName, String lastName) {
+		this.email = email;
+		this.password = password;
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 
-
-
-	public String getPhone() {
-		return phone;
+	public User(String email, String password, String username, String firstName, String lastName,
+			ArrayList<Request> myRequests) {
+		this.email = email;
+		this.password = password;
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.myRequests = myRequests;
 	}
 
-
-
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public String getFirstName() {
+		return firstName;
 	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public ArrayList<Request> getMyRequests() {
+		return myRequests;
+	}
+
+	public void setMyRequests(ArrayList<Request> myRequests) {
+		this.myRequests = myRequests;
+	}
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
-
-	public boolean isConnected() {
-		return isConnected;
-	}
-
-
-
-	public void setConnected(boolean isConnected) {
-		this.isConnected = isConnected;
-	}
-
-
-
-	public Role getRole() {
-		return role;
-	}
-
-
-
-	public void setPermission(Role role) {
-		this.role = role;
-	}
-
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
 
 	public String getUsername() {
 		return username;
 	}
 
-
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-
-	public enum Role {
-	Studint, Secturer, Employee;
-    }
 }
