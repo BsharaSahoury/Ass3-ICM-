7bb2a55dd76b0b0b3779bee9c7c8943bddd5787b
aimanouda2309 <48187816+aimanouda2309@users.noreply.github.com>
2020-01-14 17:41:12 +0200

    1

---
+++ ICM/ICM/src/Server/loginHandler.java
@@ -0,0 +1,43 @@
+package Server;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Observable;
+import java.util.Observer;
+
+import DBconnection.mysqlConnection;
+import Entity.User;
+import ocsf.server.ConnectionToClient;
+public class loginHandler implements Observer {
+		public loginHandler(Observable server) {
+			server.addObserver(this);
+		}
+
+		@Override
+		public void update(Observable o, Object arg) {
+			Object[] args = null;
+			if(arg instanceof Object[]) {
+			args=(Object[])arg;
+			ConnectionToClient client=(ConnectionToClient)args[0];
+		
+			if(args[1] instanceof String[]) {
+				String[] Message=(String[])args[1];
+				if(Message.length==3 && Message[0].equals("login")) {
+					Connection con=mysqlConnection.makeAndReturnConnection();
+					
+					User user=mysqlConnection.isInDB(con, Message[1], Message[2]);
+					
+					try {
+						client.sendToClient(user);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+			}
+			
+			}
+		}
+
+}
