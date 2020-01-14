2bc5497c90fb7be9d20c0d2448ed311d454f721d
aimanouda2309 <48187816+aimanouda2309@users.noreply.github.com>
2020-01-14 13:59:26 +0200

    12

--- ICM/src/Boundary/MainClientController.java
+++ ICM/src/Boundary/MainClientController.java
@@ -93,17 +93,12 @@ public class MainClientController {
 			ClientMapObserver cmo=new ClientMapObserver(cc.getClient());
 			ClientInitiatorapprovedrequestdecisionObserver qqw=new ClientInitiatorapprovedrequestdecisionObserver(cc.getClient());
 			ClientRejectRequestMessageSendToInitiatorObserver reject=new ClientRejectRequestMessageSendToInitiatorObserver(cc.getClient());		
-<<<<<<< HEAD
-			//ClientApproveDuratinObserver approveDuratin=new ClientApproveDuratinObserver(cc.getClient());
-			//ClientCheckApproveDuratinObserver check= new ClientCheckApproveDuratinObserver(cc.getClient());
-=======
 			ClientApproveDuratinObserver approveDuratin=new ClientApproveDuratinObserver(cc.getClient());
 			ClientCheckApproveDuratinObserver check= new ClientCheckApproveDuratinObserver(cc.getClient());
 			ClientLogOutObserver logout = new ClientLogOutObserver(cc.getClient());
 			ClientInspectorfreazerequestObserver change=new ClientInspectorfreazerequestObserver(cc.getClient());
 			ClientGetEvaluationReportObserver evaluat=new ClientGetEvaluationReportObserver(cc.getClient());
 			ClientServerDisconnectedObserver Disscon=new ClientServerDisconnectedObserver(cc.getClient());
->>>>>>> 868c922d0b0461bf9232c4d7650c660341680341
 			cc.getClient().openConnection();
 			wrong.setVisible(false);
 			login=new LoginController();
