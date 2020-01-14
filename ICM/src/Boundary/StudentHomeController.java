ff540de6ac2885efcb5a8a381b77e924fe21c6a4
samishekh <samisnow97@gmail.com>
2020-01-14 17:14:52 +0200

    kbkvkgkbkj

---
+++ ICM/src/Boundary/StudentHomeController.java
@@ -0,0 +1,118 @@
+package Boundary;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Entity.Student;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class StudentHomeController implements Initializable {
+	@FXML
+	private Button notifications;
+	@FXML
+	private Button Homebtn;
+	@FXML
+	private Button RequestWorkedOnbtn;
+	@FXML
+	private Button RequestSubmissionbtn;
+	@FXML
+	private Button ProfileSettingbtn;
+	@FXML
+	private Button AboutICMbtn;
+	@FXML
+	private ComboBox Usercombobtn;	
+	@FXML
+	private SplitPane splitpane ;
+	@FXML
+    private AnchorPane lowerAnchorPane;
+	@FXML
+	private MenuButton UserNameMenu;
+	public static Stage primaryStage;
+	private static Student student;
+	public static MyRequestsController MyRequests;
+	public void start(Student student) {
+		this.student=student;
+		primaryStage=LoginController.primaryStage;
+		Platform.runLater(new Runnable() {
+
+			@Override
+			public void run() {
+		try{
+			System.out.println("ss");
+			Parent root = FXMLLoader.load(getClass().getResource("/Boundary/Student-Home.fxml"));
+		
+			Scene scene = new Scene(root);		
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("ICM-Home");			
+			primaryStage.show();
+			primaryStage.setOnCloseRequest( event ->
+		    {
+				System.out.println("EXIT ICM");
+				LogOutController logOut = new LogOutController();
+				logOut.exit(primaryStage,student);
+		    });			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}		
+	}
+		});
+	}
+public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+public void GoToHome(ActionEvent event) throws Exception {
+	try {
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/Boundary/Home.fxml"));
+		lowerAnchorPane = loader.load();
+		splitpane.getItems().set(1, lowerAnchorPane);
+	} catch (IOException e) {		
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+
+
+public void RequestSubmissionAction(ActionEvent event) throws Exception {
+	RequestSubmissionController Submit=new RequestSubmissionController();
+	Submit.start(splitpane,student);
+}
+
+public void ProfileSettingAction(ActionEvent event) throws Exception {
+	ProfileSettingController Submit=new ProfileSettingController();
+	Submit.start(splitpane,student);
+}
+public void MyRequestsAction(ActionEvent event) throws Exception {
+	MyRequests = new MyRequestsController();
+	MyRequests.start(splitpane, student,"Student");
+}
+public void AboutICMAction(ActionEvent event) throws Exception {
+	AboutICMController about=new AboutICMController();
+	about.start(splitpane);
+}
+public void LogOutAction(ActionEvent event) throws Exception {
+	LogOutController logOut = new LogOutController();
+	primaryStage.close();
+	logOut.start(primaryStage,student);
+}
+@Override
+public void initialize(URL location, ResourceBundle resources) {
+	// TODO Auto-generated method stub
+	UserNameMenu.setText(student.getFirstName()+student.getLastName());
+}
+
+}
